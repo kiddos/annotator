63960e03efd5c62925128d339e586edaf716d0f4
kiddos <kiddo831007@gmail.com>
2018-10-13 08:38:47 +0800

    add panel component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PanelComponent } from './panel/panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule

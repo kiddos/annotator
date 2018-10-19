150de7ad280f2b5b97c69396bbdffda3b3b54960
kiddos <kiddo831007@gmail.com>
2018-10-20 00:59:57 +0800

    save categories

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@angular/core';
+import { ConfigurationService } from './configuration.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
-  constructor() {
+  categories: Array<string>;
+  selected: string;
+
+  constructor(private configurationService: ConfigurationService) {
     this.categories = [];
     this.selected = '';
+
+    this.configurationService.load(() => {
+      if (this.configurationService.data.categories) {
+        this.categories = this.configurationService.data.categories;
+      }
+    });
   }
 
   addCategory(name: string) {
     if (this.categories.indexOf(name.toLowerCase()) < 0) {
       this.categories.push(name.toLowerCase());
+      this.configurationService.add('categories', this.categories);
 
       if (!this.selected) {
         this.selected = name.toLowerCase();
@@ -23,13 +34,11 @@ export class CategoryService {
     let index = this.categories.indexOf(name.toLowerCase());
     if (index >= 0) {
       this.categories.splice(index, 1);
+      this.configurationService.add('categories', this.categories);
     }
   }
 
   selectCategory(name: string) {
     this.selected = name;
   }
-
-  categories: Array<string>;
-  selected: string;
 }
